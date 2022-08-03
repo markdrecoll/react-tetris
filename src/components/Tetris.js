a969cbef8183db48cc8a8f5083503bebbdc679f4
mdre797 <markdrecoll@gmail.com>
2022-08-03 17:05:24 -0500

    Collision detection on bottom now works. Game over working.

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -20,7 +20,7 @@ const Tetris = () => {
     const [gameOver, setGameOver] = useState(false);
 
     const [player, updatePlayerPos, resetPlayer] = usePlayer();
-    const [stage, setStage] = useStage(player);
+    const [stage, setStage] = useStage(player, resetPlayer);
     
     console.log('re-render');
 
@@ -35,10 +35,20 @@ const Tetris = () => {
         // Reset everything
         setStage(createStage());
         resetPlayer();
+        setGameOver(false);
     }
 
     const drop = () => {
-        updatePlayerPos({ x: 0, y: 1, collided: false });
+        if(!checkCollision(player, stage, { x: 0, y: 1 })){
+            updatePlayerPos({ x: 0, y: 1, collided: false });
+        }else{
+            if(player.pos.y < 1){
+                console.log("Game Over");
+                setGameOver(true);
+                setDropTime(null);
+            }
+            updatePlayerPos({ x: 0, y: 0, collided: true })
+        }
     }
 
     const dropPlayer = () => {
